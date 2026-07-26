34a324d5850e6f891572b3f9899e6a3d00c0795b
rendleks <0rendl050@gmail.com>
2026-07-26 17:45:58 +0300

    update project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
-    id("java")
+    application
+    checkstyle
+    jacoco
+    id("org.sonarqube") version "7.3.0.8198"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
@@ -13,6 +18,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+checkstyle {
+    toolVersion = "10.12.0"
 }
 
 tasks.test {
