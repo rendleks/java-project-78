cb406e1ad2c8b101b387ce2b8cecf137831c3495
rendleks <0rendl050@gmail.com>
2026-08-03 23:32:08 +0300

    add Validator

--- app/src/main/java/hexlet/code/Schema.java
+++
@@ -1,9 +0,0 @@
-package hexlet.code;
-
-public interface Schema {
-    Schema required();
-    Schema minLength(int length);
-    Schema contains(String subString);
-    boolean isValid(String text);
-
-}
