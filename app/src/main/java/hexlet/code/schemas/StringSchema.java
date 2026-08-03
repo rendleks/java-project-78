2b0143621b82b3145eddbd234b7758725338aa01
rendleks <0rendl050@gmail.com>
2026-08-03 23:32:46 +0300

    Add StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,38 +1,28 @@
 package hexlet.code.schemas;
 
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Predicate;
-import hexlet.code.Schema;
+import hexlet.code.BaseSchema;
 
 
-public class StringSchema implements Schema {
-
-    private Map<String, Predicate<String>> fluent;
+public class StringSchema extends BaseSchema<String> {
 
     public StringSchema() {
-        this.fluent = new HashMap<>();
+        super();
     }
 
     public StringSchema required() {
-        fluent.put("checkNull", text -> text != null);
-        fluent.put("lengthZero", text -> text.length() > 0);
+        super.setCheck("checkNull", text -> text != null);
+        super.setCheck("lengthZero", text -> text.length() > 0);
         return this;
     }
 
     public StringSchema minLength(int length) {
-        fluent.put("minLength", text -> text.length() > length);
+        super.setCheck("minLength", text -> text.length() > length);
         return this;
     }
 
-    public StringSchema contains(String subString) {
-        fluent.put("contains", text -> text.contains(subString));
+    public BaseSchema<String> contains(String subString) {
+        super.setCheck("contains", text -> text.contains(subString));
         return this;
     }
 
-    public boolean isValid(String value) {
-        return fluent.entrySet().stream()
-                .allMatch(s -> s.getValue().test(value));
-    }
 }
