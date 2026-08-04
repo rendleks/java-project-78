ac5fc2c1b60394976a66d890b5e6076580dfd48c
rendleks <0rendl050@gmail.com>
2026-08-04 22:36:22 +0300

    add methods

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -6,4 +6,17 @@ import java.util.Map;
 
 public class MapSchema  extends BaseSchema<Map> {
 
+    public MapSchema() {
+        super();
+    }
+
+    public MapSchema required() {
+        super.setCheck("checkNull", s -> s != null);
+        return this;
+    }
+
+    public MapSchema sizeof(int size) {
+        super.setCheck("checkSize", s -> Integer.compare(s.size(), size) == 0);
+        return this;
+    }
 }
