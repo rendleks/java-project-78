a04dbd22e9e67eee6565884f7f8f04de7f46167b
rendleks <0rendl050@gmail.com>
2026-08-25 21:50:57 +0300

    add testMapValidator

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -6,22 +6,17 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static  org.assertj.core.api.Assertions.assertThat;
 
 public class ValidatorTest {
 
-    private static Validator validator;
-
-    @BeforeAll
-    public static void beforeAllTest() {
-        validator = new Validator();
-    }
-
     @Test
     public void testStringSchema() {
-        assertThat(validator).isInstanceOf(Validator.class);
-        var schema = validator.string();
+        var v = new Validator();
+        assertThat(v).isInstanceOf(Validator.class);
+        var schema = v.string();
         assertThat(schema).isInstanceOf(StringSchema.class);
         assertThat(schema.isValid(null)).isEqualTo(true);
         assertThat(schema.isValid("")).isEqualTo(true);
@@ -39,13 +34,14 @@ public class ValidatorTest {
 
         assertThat(schema.isValid("what does the fox say")).isEqualTo(false);
 
-        var schema1 = validator.string();
+        var schema1 = v.string();
         assertThat(schema1.minLength(10).minLength(4).isValid("Hexlet")).isEqualTo(true);
     }
 
     @Test
     public void testNumberSchema() {
-        var schema2 = validator.number();
+        var v = new Validator();
+        var schema2 = v.number();
         assertThat(schema2).isInstanceOf(NumberSchema.class);
 
         assertThat(schema2.isValid(5)).isEqualTo(true);
@@ -69,21 +65,43 @@ public class ValidatorTest {
     }
 
     @Test
-    public void testMapSchema() {
-        var schema3 = validator.map();
+    public void testMapValidator() {
+        var v = new Validator();
+        var schema = v.map();
 
-        assertThat(schema3.isValid(null)).isEqualTo(true);
-        schema3.required();
+        assertThat(schema.isValid(null)).isTrue();
+        assertThat(schema.isValid(new HashMap<>())).isTrue();
 
-        assertThat(schema3.isValid(null)).isEqualTo(false);
-        assertThat(schema3.isValid(new HashMap())).isEqualTo(true);
+        schema.required();
+        assertThat(schema.isValid(null)).isFalse();
+        assertThat(schema.isValid(new HashMap<>())).isTrue();
 
-        var data = new HashMap<String, String>();
-        data.put("key1", "value1");
-        assertThat(schema3.isValid(data)).isEqualTo(true);
-        schema3.sizeof(2);
-        assertThat(schema3.isValid(data)).isEqualTo(false);
-        data.put("key2", "value2");
-        assertThat(schema3.isValid(data)).isEqualTo(true);
+        schema.sizeof(2);
+        assertThat(schema.isValid(new HashMap<>())).isFalse();
+        Map<String, String> actual1 = new HashMap<>();
+        actual1.put("key1", "value1");
+        assertThat(schema.isValid(actual1)).isFalse();
+        actual1.put("key2", "value2");
+        assertThat(schema.isValid(actual1)).isTrue();
+
+        Map<String, BaseSchema<String>> schemas = new HashMap<>();
+        schemas.put("firstName", v.string().required().contains("ya"));
+        schemas.put("lastName", v.string().required().contains("ov"));
+        schema.shape(schemas);
+
+        Map<String, String> actual2 = new HashMap<>();
+        actual2.put("firstName", "Kolya");
+        actual2.put("lastName", "Ivanov");
+        assertThat(schema.isValid(actual2)).isTrue();
+
+        Map<String, String> actual3 = new HashMap<>();
+        actual3.put("firstName", "Maya");
+        actual3.put("lastName", "Krasnova");
+        assertThat(schema.isValid(actual3)).isTrue();
+
+        Map<String, String> actual4 = new HashMap<>();
+        actual4.put("firstName", "John");
+        actual4.put("age", "B");
+        assertThat(schema.isValid(actual4)).isFalse();
     }
 }
