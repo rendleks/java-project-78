7d54a243360cb697260637c85ecbcc24a63a87b4
rendleks <0rendl050@gmail.com>
2026-09-20 22:13:40 +0300

    update MapSchema

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
