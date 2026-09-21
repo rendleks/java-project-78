c3599903dfd785eaccf195a8e212c9550379778a
rendleks <0rendl050@gmail.com>
2026-09-21 22:02:20 +0300

    update gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,18 +20,38 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
-sonar {
-  properties {
-    property("sonar.projectKey", "rendleks_java-project-78")
-    property("sonar.organization", "rendleks")
-  }
-}
-
-
 checkstyle {
     toolVersion = "10.12.0"
 }
 
+jacoco {
+    toolVersion = "0.8.14"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "rendleks_java-project-78")
+    property("sonar.organization", "rendleks")
+
+    val buildDir = project.layout.buildDirectory.get().asFile
+    property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacocoTestReport.xml")
+  }
+}
+
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.named("sonar") {
+    dependsOn(tasks.jacocoTestReport)
 }
